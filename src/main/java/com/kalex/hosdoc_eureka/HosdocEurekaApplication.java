f1a887dbbc1eda2058bd8f3d50622a55652bc0c1
G Karthik <gollak64@gmail.com>
2025-11-10 19:20:13 +0530

    initial commit with basic curls working

---
+++ src/main/java/com/kalex/hosdoc_eureka/HosdocEurekaApplication.java
@@ -0,0 +1,15 @@
+package com.kalex.hosdoc_eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class HosdocEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HosdocEurekaApplication.class, args);
+	}
+
+}
